496884a43933d0fd3cca222a26ab54692d624871
indexhtml821 <isaacopine@gmail.com>
2024-10-25 09:22:29 -0600

    resets path

--- ajaj/vue.config.js
+++ ajaj/vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'titanic.ecci.ucr.ac.cr/~ec03640/vue2/ajaj/dist/' 
+  publicPath: 'https://titanic.ecci.ucr.ac.cr/~ec03640/vue2/ajaj/dist/' 
 });
